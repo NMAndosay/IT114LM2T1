d48831b181a4262071241c0518c8202910b7cb0f
Noela Mae <andosayella13@gmail.com>
2024-03-03 13:52:26 +0800

    wrong sync folder

--- draft_mod3/draftjs.js
+++
@@ -1,8 +0,0 @@
-//carousel-image-slider
-let prev = document.querySelectorAll('.prev');
-let next = document.querySelectorAll('.next');
-
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-});
